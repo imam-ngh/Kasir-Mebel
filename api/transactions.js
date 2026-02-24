6149286680d1f896294b5f541787179370f18cac
Lr Nandra Pratama <nanda.pratama@iconpln.co.id>
2026-02-24 21:51:15 +0700

    Fix vercel.json configuration

--- api/transactions.js
+++ api/transactions.js
@@ -1,4 +1,4 @@
-import { sql } from '@vercel/postgres';
+import { db, sql } from '@vercel/postgres';
 
 export default async function handler(request, response) {
     // Izinkan CORS untuk pengembangan lokal
